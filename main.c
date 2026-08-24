c6554640d14e27e3cb4bacf3f7674981f5fb659c
pedrovcb <pedrovcb@gmail.com>
2026-08-23 23:02:49 -0300

    feat(main): add link com as implementações do executor.c/.h

--- main.c
+++ main.c
@@ -3,6 +3,44 @@
 #include <string.h>
 #include "parser.h"
 #include "task.h"
+#include "executor.h"
+
+
+void handleTask(TaskStore *store, Command *cmd) {
+    if (cmd->argc < 2) {
+        fprintf(stderr, "Uso: task <nome> <programa> [argumentos...]\n");
+        return;
+    }
+
+    int result = taskAdd(store, cmd->argv[0], &cmd->argv[1], cmd->argc - 1);
+    if (result == -1) {
+        Task *existing = taskFind(store, cmd->argv[0]);
+        if (existing != NULL) {
+            fprintf(stderr, "Erro: tarefa '%s' já existe.\n", cmd->argv[0]);
+        } else {
+            fprintf(stderr, "Erro: falha ao cadastrar tarefa '%s'.\n", cmd->argv[0]);
+        }
+    }
+}
+
+void handleRun(TaskStore *store, Command *cmd) {
+    if (cmd->argc < 2) {
+        fprintf(stderr, "Uso: run <sequential|parallel> <tarefa1> [tarefa2...]\n");
+        return;
+    }
+
+    char *mode = cmd->argv[0];
+    char **taskNames = &cmd->argv[1];
+    int taskCount = cmd->argc - 1;
+
+    if (strcmp(mode, "sequential") == 0) {
+        runSequential(store, taskNames, taskCount);
+    } else if (strcmp(mode, "parallel") == 0) {
+        runParallel(store, taskNames, taskCount);
+    } else {
+        fprintf(stderr, "Uso: run <sequential|parallel> <tarefa1> [tarefa2...]\n");
+    }
+}
 
 void loopInterativo(TaskStore *store) {
     char line[1024];
@@ -34,9 +72,17 @@ void loopInterativo(TaskStore *store) {
             break;
         }
 
-        // espaço pra ter os comandos dps sla
-
-        printf("Comando não implementado ainda.\n");
+        switch (cmd.type) {
+            case CMD_TASK:
+                handleTask(store, &cmd);
+                break;
+            case CMD_RUN:
+                handleRun(store, &cmd);
+                break;
+            default:
+                fprintf(stderr, "Comando desconhecido: %s\n", cmd.argv[0]);
+                break;
+        }
 
         commandFree(&cmd);
     }
@@ -72,8 +118,17 @@ void loopWorkflow(TaskStore *store, const char *filename) {
             break;
         }
 
-        // tbm espaço pra os comandos dps
-        printf("Comando não implementado ainda.\n");
+        switch (cmd.type) {
+            case CMD_TASK:
+                handleTask(store, &cmd);
+                break;
+            case CMD_RUN:
+                handleRun(store, &cmd);
+                break;
+            default:
+                fprintf(stderr, "Comando desconhecido: %s\n", cmd.argv[0]);
+                break;
+        }
 
         commandFree(&cmd);
     }
